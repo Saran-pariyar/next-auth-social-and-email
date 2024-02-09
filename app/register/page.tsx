5ddae3ccd21f2674933b390cd202edf954e3354f
Saran Pariyar <spariyar05@gmail.com>
2024-02-09 22:23:37 +0545

    created form for register

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,9 +1,48 @@
 import React from 'react'
+import Link from "next/link";
 
 const Register = () => {
   return (
-    <div>Register</div>
+    <div className="flex min-h-screen flex-col items-center justify-between p-24">
+    <div className="bg-[#212121] p-8 rounded shadow-md w-96">
+      <h1 className="text-4xl text-center font-semibold mb-8">Register</h1>
+      <form 
+    //   onSubmit={handleSubmit}
+      >
+        <input
+          type="text"
+          className="w-full border border-gray-300 text-black rounded px-3 py-2 mb-4 focus:outline-none focus:border-blue-400 focus:text-black"
+          placeholder="Email"
+          required
+        />
+        <input
+          type="password"
+          className="w-full border border-gray-300 text-black rounded px-3 py-2 mb-4 focus:outline-none focus:border-blue-400 focus:text-black"
+          placeholder="Password"
+          required
+        />
+        <button
+          type="submit"
+          className="w-full bg-blue-500 text-white py-2 rounded hover:bg-blue-600"
+        >
+          {" "}
+          Register
+        </button>
+        <p className="text-red-600 text-[16px] mb-4">
+            {/* {error && error} */}
+            </p>
+      </form>
+      <div className="text-center text-gray-500 mt-4">- OR -</div>
+      <Link
+        className="block text-center text-blue-500 hover:underline mt-2"
+        href="/login"
+      >
+        Login with an existing account
+      </Link>
+    </div>
+  </div>
   )
+  
 }
 
 export default Register
